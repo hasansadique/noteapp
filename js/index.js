fa319b213e7c79bf2243aaefae1654f73bf25bd0
Hasan Sadique <hasansadiq1129@gmail.com>
2022-12-01 23:09:42 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,78 @@
+const addBtn = document.querySelector("#addBtn")
+const main = document.querySelector("#main")
+addBtn.addEventListener(
+    "click",
+    function () {
+        addNote()
+    }
+)
+const saveNotes = () => {
+    const notes = document.querySelectorAll(".note textarea");
+    const data = [];
+    notes.forEach(
+        (note) => {
+            data.push(note.value)
+        }
+    )
+    if (data.length === 0) {
+        localStorage.removeItem("notes")
+    } else {
+        localStorage.setItem("notes", JSON.stringify(data))
+    }
+}
+//   <div class="note">
+//  <div class="tool">
+//  <i class="fa-solid fa-floppy-disk"></i>
+//  <i class="fa-solid fa-trash"></i>
+// </div>
+// <textarea></textarea>
+// </div> 
+
+const addNote = (text = "") => {
+    const note = document.createElement("div");
+    note.classList.add("note")
+    note.innerHTML = `
+    <div class="tool">
+  <i class="save fa-solid fa-floppy-disk"></i>
+  <i class="trash fa-solid fa-trash"></i>
+</div>
+ <textarea>${text}</textarea>`;
+
+    note.querySelector(".trash").addEventListener(
+        "click",
+        function () {
+            note.remove();
+        }
+    )
+    note.querySelector(".save").addEventListener(
+        "click",
+        saveNotes
+    )
+    note.querySelector("textarea").addEventListener(
+        "focusout",
+        function () {
+            saveNotes()
+        }
+    )
+    main.appendChild(note);
+    saveNotes()
+}
+(
+    function () {
+        const lsNotes = JSON.parse(localStorage.getItem("notes"));
+        if (lsNotes === null) {
+            addNote()
+        } else {
+            lsNotes.forEach(
+                (lsNote) => {
+                    addNote(lsNote)
+                }
+            )
+        }
+        if (lsNotes.length === 0) {
+            localStorage.removeItem("notes")
+        } else {
+            addNote()
+        }
+    }
+)()
